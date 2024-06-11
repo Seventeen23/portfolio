aae990d8207fa35b32a2e4f03b24855f4b87af0b
Matthew Feri Tanutan <matthewmaurillo@gmail.com>
2024-06-11 19:57:27 +0800

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
+
+// Adds The Navbar to navbar div
 fetch('navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
     })
     .catch()  
+
+
